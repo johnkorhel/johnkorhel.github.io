43b158945e094d9a830e4c68cded0d8f6288e88e
John Korhel <johnsbadusername@gmail.com>
2018-12-20 15:07:49 -0600

    barba.js test

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var HideShowTransition = Barba.BaseTransition.extend({
+  start: function() {
+    this.newContainerLoading.then(this.finish.bind(this));
+  },
+
+  finish: function() {
+    document.body.scrollTop = 0;
+    this.done();
+  }
+});
+Barba.Pjax.getTransition = function() {
+  return HideShowTransition;
+};
+Barba.Prefetch.init();
+Barba.Pjax.start();
