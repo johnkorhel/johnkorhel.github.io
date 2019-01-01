577ecf64de569f75e82a4c1697325a78040f5c46
John Korhel <johnsbadusername@gmail.com>
2019-01-01 14:03:55 -0600

    remove

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,3 @@
 $('.navbar-collapse a').click(function(e) {
         $('.navbar-collapse').collapse('toggle');
     });
-    var url = window.location;
-    $('ul.nav a[href="' + url + '"]').parent().addClass('active');
-    $('ul.nav a').filter(function() {
-        return this.href == url;
-    }).parent().addClass('active');
