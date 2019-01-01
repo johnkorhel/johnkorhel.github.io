e0ea4cd0077321299854c1e65d582b4cd5212311
John Korhel <johnsbadusername@gmail.com>
2019-01-01 14:00:14 -0600

    collapse and active test

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.navbar-collapse a').click(function(e) {
+        $('.navbar-collapse').collapse('toggle');
+    });
+    var url = window.location;
+    $('ul.nav a[href="' + url + '"]').parent().addClass('active');
+    $('ul.nav a').filter(function() {
+        return this.href == url;
+    }).parent().addClass('active');
+});
